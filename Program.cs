e54e8d1f12c2a4eb3800899dec7d6cbbfe684ae2
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-03-24 13:29:53 +0300

    Create class Item

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+class Item
+{
+    public string Name { get; }
+    private int Quantity { get; set; }
+
+    private DateTime CreatedDate { get; set; }
+
+    public Item(string name, int quantity, DateTime createdDate)
+    {
+        Name = name;
+        Quantity = quantity;
+        CreatedDate = createdDate;
+    }
+
+
+}
