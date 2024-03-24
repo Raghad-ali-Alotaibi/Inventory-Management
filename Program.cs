a35fdf82af415066318a6a89a325755c81bea629
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-03-24 22:19:45 +0300

    update add and delete methods

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-
-    }
-}
-
 class Item
 {
     public string Name { get; }
-    private int Quantity { get; set; }
+    private int quantity;
+    public int Quantity
+    {
+        get { return quantity; }
+        set { quantity = value; }
+    }
 
     private DateTime CreatedDate { get; set; }
 
@@ -26,17 +23,76 @@ class Item
         Quantity = quantity;
         CreatedDate = createdDate == default ? DateTime.Now : createdDate;
     }
+    public override string ToString()
+    {
+        return $"Item Name: {Name} ,Quantity: {Quantity} , CreatedDate: {CreatedDate} ";
+    }
 }
 
 
-class Store {
-    private List<Item> items =new List<Item>();
+class Store
+{
+    private List<Item> items = new List<Item>();
 
-    public void AddItem(){
+    public void PrintItem()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine($"{item}");
+        }
+    }
+    public void AddItem(Item item)
+    {
+        bool isItemExist = items.Any((newItem) => newItem.Name == item.Name);
+        if (isItemExist)
+        {
+            Console.WriteLine("Item with the same name already exists in the store.");
+            return;
+        }
+        items.Add(item);
 
     }
-    public void DeleteItem(){
-
+    public void DeleteItem(string itemName)
+    {
+        Item itemToDelete = items.FirstOrDefault(item => item.Name == itemName);
+        if (itemToDelete != null)
+        {
+            items.Remove(itemToDelete);
+            Console.WriteLine("Item was Deleted");
+        }
+        else
+        {
+            Console.WriteLine("Item not found in the store.");
+        }
     }
 
+    public int GetCurrentVolume()
+    {
+        return items.Sum(item => item.Quantity);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var waterBottle = new Item("Water Bottle", 10, new DateTime(2023, 1, 1));
+        var chocolateBar = new Item("Chocolate Bar", 15, new DateTime(2023, 2, 1));
+        var notebook = new Item("Notebook", 5, new DateTime(2023, 3, 1));
+        var pen = new Item("Pen", 20, new DateTime(2023, 4, 1));
+
+
+        var store = new Store();
+
+        store.AddItem(waterBottle);
+        store.AddItem(chocolateBar);
+        store.AddItem(notebook);
+        store.AddItem(pen);
+
+        store.PrintItem();
+
+        store.DeleteItem("Water Bottle 1");
+        Console.WriteLine($"{store.GetCurrentVolume()}");
+    }
 }
